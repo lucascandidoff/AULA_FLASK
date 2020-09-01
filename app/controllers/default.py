16a05383943461de1b24a9dd85b209f16a5454ab
lucascandidoff <lucascandidoff@gmail.com>
2020-09-01 00:15:41 -0300

    commit

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,14 @@
 from app import app
 
+@app.route('/index')
 @app.route("/")
 def index():
-    return "Hello world!!"
+    return "Hello world!"
+
+@app.route('/test', defaults={'name': None})
+@app.route("/test/<name>")
+def test(name):
+    if name:
+        return 'Olá, %s!' %name
+    else:
+        return 'Olá, usuário!'
