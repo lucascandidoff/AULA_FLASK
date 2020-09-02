4661ed8f9284b34afe3b3e75817752a84bd78451
lucascandidoff <lucascandidoff@gmail.com>
2020-09-02 00:44:22 -0300

    commit

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,16 @@
+from flask import render_template
 from app import app
 
-@app.route('/index')
-@app.route("/")
-def index():
-    return "Hello world!"
+@app.route('/index/<user>')
+@app.route('/', defaults={'user':None})
+def index(user):
+    return render_template('index.html', user=user)
 
-@app.route('/test', defaults={'name': None})
-@app.route("/test/<name>")
-def test(name):
-    if name:
-        return 'Olá, %s!' %name
-    else:
-        return 'Olá, usuário!'
+
+# @app.route('/test', defaults={'name': None})
+# @app.route("/test/<name>")
+# def test(name):
+#     if name:
+#         return 'Olá, %s!' %name
+#     else:
+#         return 'Olá, usuário!'
